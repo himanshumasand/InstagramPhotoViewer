bfd7c11eaff72618e516dea8fa6df13badca9e6a
Himanshu Masand <masand@seas.upenn.edu>
2015-09-17 23:18:40 -0700

    Implementing swipe to refresh

--- InstagramPhotoViewer/app/src/main/java/himanshumasand/github/com/instagramphotoviewer/PhotoStreamActivity.java
+++ InstagramPhotoViewer/app/src/main/java/himanshumasand/github/com/instagramphotoviewer/PhotoStreamActivity.java
@@ -1,5 +1,6 @@
 package himanshumasand.github.com.instagramphotoviewer;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,8 @@ public class PhotoStreamActivity extends ActionBarActivity {
 
     private ArrayList<Photo> photos;
     private PhotosAdapter photosAdapter;
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,22 @@ public class PhotoStreamActivity extends ActionBarActivity {
         //send api request
         fetchPopularPhotos();
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchPopularPhotos();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     private void  fetchPopularPhotos() {
@@ -49,6 +68,7 @@ public class PhotoStreamActivity extends ActionBarActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 Log.i("DEBUG" , "RECEIVED: Data for popular photos: " + response.toString());
                 JSONArray photosJSON = null;
+                photos = new ArrayList<>();
                 try {
                     photosJSON = response.getJSONArray("data");
                     for(int i = 0; i < photosJSON.length(); i++) {
@@ -57,8 +77,11 @@ public class PhotoStreamActivity extends ActionBarActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
+                photosAdapter.clear();
+                photosAdapter.addAll(photos);
                 photosAdapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+
             }
 
             //onFailure
